bb58b68135df3095157dbe95faa62075be4a7b38
EDK11 <esantofinio@gmail.com>
2021-12-06 20:52:42 -0500

    Subida de API

---
+++ API-GATEWAY/src/resolvers/compra_resolver.js
@@ -0,0 +1,23 @@
+const compraResolver = {
+    Query: {
+        compraByUsername: async(_, {username}, {dataSources})=>{
+            return await dataSources.comprasAPI.compraByUsername(username);
+        },
+
+        compraById: async(_, {id}, {dataSources})=>{
+            return await dataSources.comprasAPI.compraById(id);
+        }
+    },
+    Mutation: {
+        createCompra: async (_, {compra}, {dataSources}) => {
+            const compraInput = {
+                username:compra.username,
+                id_user:compra.id_user,
+                id_orden:compra.id_orden,
+            }
+            return await dataSources.comprasAPI.createCompra(compraInput);
+        }
+    }
+}
+
+module.exports = compraResolver;
---
+++ API-GATEWAY/src/typeDefs/compra_type_defs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server');
+
+const compraTypeDefs = gql `
+    type Compra {
+        username: String!
+        id_user: Int!
+        id_orden: Int!
+    }
+
+    input CompraInput {
+        username: String!
+        id_user: Int!
+        id_orden: Int!
+    }
+
+    extend type Query {
+        compraByUsername(username: String!): [Compra]
+    }
+
+    extend type Query {
+        compraById(id: String!): Compra
+    }
+
+    extend type Mutation {
+        createCompra(compra: CompraInput!): Compra
+    }
+`;
+
+module.exports = compraTypeDefs;
